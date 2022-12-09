f6754b9e5f995a4278a710cecc038fdbc9317874
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2022-12-09 13:39:55 +0400

    change scale on transition + new images

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,9 @@ const SliderContainer = Slider.querySelector("#slider-image-container");
 const imageElement = SliderContainer.querySelector(".slider-image");
 
 const images = [
-	"https://images.unsplash.com/photo-1607604276583-eef5d076aa5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YW5pbWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-	"https://images.unsplash.com/photo-1600758208050-a22f17dc5bb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YW5pbWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-	"https://images.unsplash.com/photo-1519638399535-1b036603ac77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YW5pbWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-	"https://images.unsplash.com/photo-1601850494422-3cf14624b0b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGFuaW1lfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-	"https://images.unsplash.com/photo-1493515322954-4fa727e97985?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGFuaW1lfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+	"https://c4.wallpaperflare.com/wallpaper/695/331/660/digital-art-artwork-women-cityscape-wallpaper-preview.jpg",
+	"https://c4.wallpaperflare.com/wallpaper/291/819/697/illustration-city-anime-painting-wallpaper-preview.jpg",
+	"https://c4.wallpaperflare.com/wallpaper/401/125/293/fantasy-art-couple-sun-sunset-wallpaper-preview.jpg",
 ];
 
 let counter = 0;
@@ -15,10 +13,10 @@ const animationDuration = 200;
 const slideDuration = 2000;
 
 init();
-// autoSlide();
 
 function handleArrowClick(e) {
 	const arrow = e.dataset.arrow;
+	const transitionRotation = 5;
 	let multiplier = 0;
 
 	if (arrow == "left") {
@@ -29,11 +27,12 @@ function handleArrowClick(e) {
 		slideRight();
 	}
 	imageElement.style.opacity = "0";
-	imageElement.style.rotate = `${5 * multiplier}deg`;
-
+	imageElement.style.rotate = `${transitionRotation * multiplier}deg`;
+	imageElement.style.scale = 1.25;
 	setTimeout(() => {
 		imageElement.style.opacity = "1";
 		imageElement.style.rotate = "0deg";
+		imageElement.style.scale = 1;
 	}, animationDuration);
 }
 
